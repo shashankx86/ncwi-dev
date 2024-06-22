1317cbb6cb0a028a12ffcee891377a05744d1f87
shashankx86 <shashankspis20@gmail.com>
2024-06-22 20:06:09 +0000

    init frontend

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <Navbar />
+    <div class="content">
+      <h1>NCWI</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+import Navbar from './components/Navbar.vue';
+import './style.css';
+
+export default {
+  name: 'App',
+  components: {
+    Navbar
+  }
+}
+</script>
+
---
+++ src/components/Navbar.vue
@@ -0,0 +1,20 @@
+<template>
+  <nav class="navbar">
+    <div class="navbar-left">
+      <button class="navbar-item">Services</button>
+      <button class="navbar-item">Terminal</button>
+      <button class="navbar-item">Settings</button>
+    </div>
+    <div class="navbar-right">
+      <button class="navbar-item">
+        <img src="../assets/logo.png" alt="Session Icon" class="session-icon">
+      </button>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+}
+</script>
