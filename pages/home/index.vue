8d652b1b49d607af9e4b003dedad910cab09b5ef
shashankx86 <shashankspis20@gmail.com>
2024-06-29 17:48:46 +0000

    set content to be in full page

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
       <Navbar />
-      <div class="content">
+      <div class="content bg-neutral-700 flex-[1] flex flex-col mt-12 min-h-screen">
         <h1>Welcome to the Home Page</h1>
       </div>
     </div>
@@ -10,10 +10,3 @@
   <script setup lang="ts">
   import Navbar from '@/components/Navbar.vue';
   </script>
-  
-  <style scoped>
-  .content {
-    padding-top: 60px;
-  }
-  </style>
-  
