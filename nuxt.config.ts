86393f4f2d746666a9c4ca75e7ebd7ff5ad452c0
shashankx86 <shashankspis20@gmail.com>
2024-06-29 06:08:08 +0000

    re:init project in nuxt

---
+++ nuxt.config.ts
@@ -0,0 +1,8 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: ["@nuxtjs/tailwindcss", "shadcn-nuxt"],
+  shadcn: {
+    componentDir: './components/ui'
+  }
+})
